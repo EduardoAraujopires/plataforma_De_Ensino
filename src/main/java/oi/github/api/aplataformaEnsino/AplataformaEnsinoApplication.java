1bc52324e6635fd8194a512b63864370c48e64c8
Eduardo Araújo <eduaraujopires370@gmail.com>
2026-03-08 19:04:54 -0300

    Projeto criado

---
+++ src/main/java/oi/github/api/aplataformaEnsino/AplataformaEnsinoApplication.java
@@ -0,0 +1,13 @@
+package oi.github.api.aplataformaEnsino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplataformaEnsinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplataformaEnsinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oi/github/api/aplataformaEnsino/AplataformaEnsinoApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.github.api.aplataformaEnsino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplataformaEnsinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
